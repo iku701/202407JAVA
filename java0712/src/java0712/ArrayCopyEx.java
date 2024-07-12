d6ab67b7aa872b3b9d13dd5d89f5b94c4e53078d
iku701 <iku701@naver.com>
2024-07-12 17:45:41 +0900

    commit

---
+++ java0712/src/java0712/ArrayCopyEx.java
@@ -0,0 +1,20 @@
+package java0712;
+
+public class ArrayCopyEx {
+    public static void main(String[] args) {
+        String[] oldStrArray = {"java", "array", "copy"};
+        
+        String[] newStrArray = new String[5];
+        
+        System.arraycopy(oldStrArray, 0, newStrArray, 0, oldStrArray.length);
+        
+        for (int i = 0; i < newStrArray.length; i++) {
+            if (newStrArray[i] != null) {
+                System.out.print(newStrArray[i]);
+                if (i < newStrArray.length - 1 && newStrArray[i + 1] != null) {
+                    System.out.print(", ");
+                }
+            }
+        }
+    }
+}
--- project1/src/project1/Project1.java
+++ project1/src/project1/Project1.java
@@ -1,11 +1,35 @@
 package project1;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
 public class Project1 {
 
 	public static void main(String[] args) {
-		Member member = null;
+//		Member[] list = null;
+		Member[] list = new Member[10];  // 총 회원수 10명으로 제한
+		int memCnt = 0;   // 회원수
+		try (FileInputStream fis = new FileInputStream("c:\\temp\\members.dat");
+	             ObjectInputStream ois = new ObjectInputStream(fis)) {
+			Member[] list2 = (Member[]) ois.readObject();
+			System.arraycopy(list2, 0, list, 0, list2.length);  // 우리 자바책 p219
+			System.out.println("파일에서 객체를 가져왔습니다.");
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		for (Member member : list) {
+			if (member != null) {
+				System.out.println(member);
+				++memCnt;
+			}
+		}
+		System.out.println("총회원수:"+memCnt);
+		
+		Member member = null; // 로그인 된 현재 사용자
 		Scanner scanner = new Scanner(System.in);
 		boolean run = true;
 		while (run) {
@@ -23,15 +47,28 @@ public class Project1 {
 				String name = scanner.nextLine(); // name 변수의 값 입력
 				System.out.print("패스워드:");
 				String strPassword = scanner.nextLine();//패스워드입력
-				if (name.equals(member.name)) {
-					if (strPassword.equals(member.ssn)) {
-						System.out.println("로그인 성공");
-					} else {
-						System.out.println("로그인 실패:패스워드가 틀림");
+				
+				int find = -1;  // 찾기 전 또는 못찾았을 때
+				
+				for (int i=0; i < list.length; i++) {
+					if (list[i].name.equals(name) && list[i].ssn.equals(strPassword)) {
+						find = i;
+						member = list[i];
+						break;
 					}
-				} else {
-					System.out.println("로그인 실패:아이디 존재하지 않음");
 				}
+				System.out.println("인덱스:"+find);
+				System.out.println(member);
+				
+//				if (name.equals(member.name)) {
+//					if (strPassword.equals(member.ssn)) {
+//						System.out.println("로그인 성공");
+//					} else {
+//						System.out.println("로그인 실패:패스워드가 틀림");
+//					}
+//				} else {
+//					System.out.println("로그인 실패:아이디 존재하지 않음");
+//				}
 				break;
 			case 2:
 				// 회원 가입
@@ -50,7 +87,7 @@ public class Project1 {
 				System.out.println("2. 주민번호 앞 6자리: " + ssn); 
 				System.out.println("3. 전화번호: " + tel);
 				// 객체 생성
-				member = new Member(name2, ssn, tel);
+				list[memCnt++] = new Member(name2, ssn, tel);
 				break;
 			case 3:
 				// 예금 출금
@@ -85,6 +122,16 @@ public class Project1 {
 				System.out.println("예금/출금 프로그램 종료");
 				break;
 			case 4:
+				try (FileOutputStream fos = new FileOutputStream("c:\\temp\\members.dat");
+			             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+					
+					 oos.writeObject(list);
+			         System.out.println("객체를 파일에 저장했습니다.");
+					
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 				run = false;
 				break;
 			}
